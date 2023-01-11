76b6ec43c48262c0687c8598f836a17e0f3e8ade
PAI-PAI1014 <121843983+PAI-PAI1014@users.noreply.github.com>
2023-01-11 14:06:45 +0700

    multi page101
    Hello Dev

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 //const color_text = require('chalk');
 const debug = require('debug')('app');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+const path = require('path')
 
-app.get("/", (req,res) =>{
-    res.send("Hello DevPAI 555");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views","./src/views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render('index',{username: 'PaiZa555+'});
 })
 
-app.listen(port, ()=>{
-    debug("Litening on port ", port);
+app.listen(PORT, () => {
+    debug("Litening on port ", PORT);
 })
